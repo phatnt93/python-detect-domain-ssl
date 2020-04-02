ea530f9f333b9406bc9cd6a598997c4be2e3358a
Thanh Phat <thanhphat.uit@gmail.com>
2020-04-02 20:52:30 +0700

    Update DetectDomainSSL.py

--- DetectDomainSSL.py
+++ DetectDomainSSL.py
@@ -1,20 +1,20 @@
 # 
 # Detect SSL Domain Expired
 # 
-# Made by Phatnt
+# Made by phatnt93
 # 01/04/2020
 # 
 # Require package
 # pip install pyOpenSSL
 # 
-# CMD: python kov_detect_domain_ssl.py -d [domain name] -f [file path contain list domain] -a [days before expired]
+# CMD: python DetectDomainSSL.py -d [domain name] -f [file path contain list domain] -a [days before expired]
 # EX:
-# python kov_detect_domain_ssl.py -d google.com -a 30
-# python kov_detect_domain_ssl.py -f domains.txt -a 30
+# python DetectDomainSSL.py -d google.com -a 30
+# python DetectDomainSSL.py -f domains.txt -a 30
 # 
 # @license MIT License
 # @author phatnt <thanhphat.uit@gmail.com>
-# @github https://github.com/phatnt93/python_detect_domain_ssl
+# @github https://github.com/phatnt93/python-detect-domain-ssl
 # @version 1.0.0
 # 
 
